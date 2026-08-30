588bc00feeb2ffd597ae00e5446085680ed5118c
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-08-30 13:48:30 -0400

    UPLOAD CODE

---
+++ Upload to GitHub.sh
@@ -0,0 +1,8 @@
+cd C:/Users/cryst/Documents/GitHub/PotentiaMod/extensions
+git switch main
+git init
+git add .
+git commit -m "UPLOAD CODE"
+git branch -M main
+git remote add origin https://github.com/PotentiaMod/extensions.git
+git push -f origin main
